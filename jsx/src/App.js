7ecc1659b5b975eac63a2d2a74fa0f4b80d335da
yoannet93 <yoannet93@gmail.com>
2023-06-13 22:55:40 +0200

    add style

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,19 +1,49 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import alexaImage from "./images/alexa.png";
 import cortanaImage from "./images/cortana.png";
 import siriImage from "./images/siri.png";
 
-console.log(alexaImage)
-console.log(cortanaImage);
-console.log(siriImage);
-
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistant</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image={alexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana" image={cortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={siriImage} />
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <p class="title">Title of the page</p>
+          <p class="subtitle">Primary subtitle</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div class="columns">
+            <div class="column is-2">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={alexaImage}
+                description="Alexa was created by Amazon and helps you buy things"
+              />
+            </div>
+            <div class="column is-2">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana"
+                image={cortanaImage}
+                description="Cortana was made by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div class="column is-2">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={siriImage}
+                description="Siri was made by Apple and is being phase out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- jsx/src/ProfileCard.js
+++ jsx/src/ProfileCard.js
@@ -1,11 +1,22 @@
-function ProfileCard({title, handle, image}){
-    return (
-      <div>
-        <img src={image} alt="images" />
-        <div>Title is {title}</div>
-        <div>Handle is {handle}</div>
+function ProfileCard({ title, handle, image, description}) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="images" />
+        </figure>
       </div>
-    );
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">Title is {title}</p>
+          <p className="subtitle is-6">Handle is {handle}</p>
+        </div>
+        <div>
+            {description}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard; 
