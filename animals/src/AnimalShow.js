ea0f6817838c9c24809d1113400053376c38e96d
yoannet93 <yoannet93@gmail.com>
2023-06-15 20:47:32 +0200

    update app

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,7 +1,53 @@
-function AnimalShow(){
-    return (
-        <div>Cow!</div>
-    )
+import "./AnimalShow.css";
+import { useState } from "react";
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import horse from "./svg/horse.svg";
+import heart from "./svg/heart.svg";
+
+/*Creo un objeto para asignar a variables estas imagenes.
+ dentro del objeto, cada linea es equivalente a poner 
+
+ const svgMap = {
+    bird : bird,
+    cat : cat,
+    cow : cow,
+    dog : dog,
+    gator : gator,
+    horse : horse,
+};
+
+Cdo la propiedad y el valor es el mismo puedo abreviar el código poniendo solo el nombre.
+*/
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setclicks] = useState(0);
+  const handleClick = () => {
+    setclicks(clicks + 1);
+  };
+
+  return (
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" alt="animals" src={svgMap[type]} />
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 10 * clicks + "px" }}
+      />
+    </div>
+  );
 }
 
 export default AnimalShow;
--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,29 @@
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
 
-function App(){
-    
-    const handleEvent = () =>{
-       
-        console.log('YOU ARE PRESS THE BUTTON ')
-    }
-    return <button onClick={handleEvent}> Add Animals </button>;
+function getRandomAnimals() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+  const handleEvent = () => {
+    setAnimals([...animals, getRandomAnimals()]); //es equivalente a usar animals.push(getRandomAnimals()) pero se usa asi para no modificar el estado.
+  };
+
+  //Genera una lista de componentes segun el tipo de animal.
+  const renderedAnimals = animals.map((animals, index) => {
+    return <AnimalShow type={animals} key={index} />;
+  });
+
+  return (
+    <div className="app">
+      <button onClick={handleEvent}> Add Animals </button>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
+  );
 }
 
 export default App;
